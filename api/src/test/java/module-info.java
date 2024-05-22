648e24206a43bddf902d9d571a4d46675cad05d4
Michał Krysztofik <michal.krysztofik@kontomatik.com>
2024-05-22 10:17:30 +0200

    rm apiTest module

--- api/src/test/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module apiTest {
-  requires org.apache.httpcomponents.client5.httpclient5;
-  requires org.apache.httpcomponents.core5.httpcore5;
-  requires org.junit.jupiter.api;
-  requires spring.beans;
-  requires spring.boot.test;
-}
